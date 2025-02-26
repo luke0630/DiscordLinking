80753b6dc8241a1bce1936e565dce37729f7240a
luke0630 <ryuto-0915@outlook.jp>
2025-02-26 22:07:57 +0900

    add: SQLUtility

---
+++ src/main/java/org/luke/discordLinking/SQL/SQLUtility.java
@@ -0,0 +1,107 @@
+package org.luke.discordLinking.SQL;
+
+import lombok.experimental.UtilityClass;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+
+import static org.luke.discordLinking.SQL.SQLManager.*;
+
+@UtilityClass
+public class SQLUtility {
+    public void putData(Long discordID, String mcUUID) {
+        try {
+            JSONArray jsonArray = getLinkedDataByDiscordID(discordID);
+            if(jsonArray != null) {
+                boolean alreadyLinked = false;
+                for(int i=0;i < jsonArray.length();i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String uuid = jsonObject.getString("uuid");
+
+                    if(uuid.equals(mcUUID)) {
+                        alreadyLinked = true;
+                        break;
+                    }
+                }
+
+                if(alreadyLinked) {
+                    System.out.println("すでに紐づけ済みです uuid:" + mcUUID);
+                } else {
+                    jsonArray.put(getJSONData(mcUUID));
+
+                    String sql = "UPDATE "+ tableName +" SET "+ column_linked_data +" = ? WHERE "+ column_discordID +" = ?";
+                    PreparedStatement update = connection.prepareStatement(sql);
+
+                    update.setString(1, jsonArray.toString());
+                    update.setString(2, discordID.toString());
+
+                    update.executeUpdate();
+                }
+            } else {
+                System.out.println("存在しないため作成しました");
+                PreparedStatement insert = connection.prepareStatement(
+                        "INSERT INTO " + SQLManager.tableName + " (" + SQLManager.column_discordID + ", " + column_linked_data + ") VALUES (?, ?)"
+                );
+
+                jsonArray = new JSONArray();
+                jsonArray.put(getJSONData(mcUUID));
+
+                insert.setLong(1, discordID);
+                insert.setString(2, jsonArray.toString());
+
+                insert.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public JSONArray getLinkedDataByDiscordID(Long discordID) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM "+ SQLManager.tableName +" WHERE "+ SQLManager.column_discordID +" = ?");
+            stmt.setLong(1, discordID);
+
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()) {
+                return new JSONArray(rs.getString(column_linked_data));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public JSONObject getJSONData(String mcUUID) {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedString = formatter.format(timestamp);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("uuid", mcUUID);
+        jsonObject.put("timestamp", formattedString);
+
+        return jsonObject;
+    }
+
+    public String getDiscordIdByUUID(String mcUUID) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM "+ SQLManager.tableName);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+                JSONArray jsonArray = new JSONArray(rs.getString(column_linked_data));
+                for(int i=0;i < jsonArray.length();i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String uuid = jsonObject.getString("uuid");
+
+                    if(uuid.equals(mcUUID)) {
+                        return rs.getString(column_discordID);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+}
