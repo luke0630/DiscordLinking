1a57ecf87702c43c25511e7ccc390d24b133097a
luke0630 <ryuto-0915@outlook.jp>
2025-02-27 19:27:03 +0900

    rm: 不要なデータクラス

--- src/main/java/org/luke/discordLinking/Data.java
+++ src/main/java/org/luke/discordLinking/Data.java
@@ -1,11 +1,5 @@
 package org.luke.discordLinking;
 
-import com.velocitypowered.api.proxy.Player;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.UUID;
-
 public class Data {
 
     public static Long discordLinkedRoleID = null;
@@ -16,52 +10,4 @@ public class Data {
     public static String mysqlPassword = "";
     public static String mysqlDatabaseName = "";
     public static Integer effectiveTimeForCode = 300;
-
-    public static HashMap<UUID, AuthData> codeAuth = new HashMap<>();
-
-    public static class LinkedData {
-        private Long discordUserID = null;
-
-        public LinkedData(Long discordUserID, LocalDateTime nowDateTime) {
-            this.discordUserID = discordUserID;
-            linkedLocalDateTime = nowDateTime;
-        }
-
-        private final LocalDateTime linkedLocalDateTime;
-
-        public LocalDateTime getLinkedLocalDateTime() {
-            return linkedLocalDateTime;
-        }
-        public Long getDiscordUserID() {
-            return discordUserID;
-        }
-    }
-
-    public static class AuthData {
-        private String code = "";
-        private Player player = null;
-        private Integer resetTime = 5*60; //秒単位 これを過ぎるとコードが無効になる
-
-        public AuthData(String code, Player player, Integer restTime) {
-            this.code = code;
-            this.player = player;
-            this.resetTime = restTime;
-        }
-
-        public void setResetTime(Integer resetTime) {
-            this.resetTime = resetTime;
-        }
-
-        public Integer getResetTime() {
-            return resetTime;
-        }
-
-        public Player getPlayer() {
-            return player;
-        }
-
-        public String getCode() {
-            return code;
-        }
-    }
 }
