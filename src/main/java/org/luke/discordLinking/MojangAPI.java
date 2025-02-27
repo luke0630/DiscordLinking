1fe3d9af3b7412378c075b67cfb37009054aaa4f
luke0630 <ryuto-0915@outlook.jp>
2025-02-27 19:32:15 +0900

    update: 不要なメソッドを削除し、tryを使用

--- src/main/java/org/luke/discordLinking/MojangAPI.java
+++ src/main/java/org/luke/discordLinking/MojangAPI.java
@@ -1,5 +1,6 @@
 package org.luke.discordLinking;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -7,36 +8,33 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class MojangAPI {
-    private static String getUsernameFromUUID(String uuid) throws Exception {
-        String urlString = "https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.replace("-","");
-        URL url = new URL(urlString);
-
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-
-        // レスポンスコードを確認
-        int responseCode = connection.getResponseCode();
-        if (responseCode != HttpURLConnection.HTTP_OK) {
-            throw new Exception("Error: " + responseCode);
-        }
-
-        InputStreamReader reader = new InputStreamReader(connection.getInputStream());
-        JsonObject responseJson = JsonParser.parseReader(reader).getAsJsonObject();
-
-        // プロファイル名が存在するか確認
-        if (responseJson.has("name")) {
-            return responseJson.get("name").getAsString();
-        } else {
-            throw new Exception("Profile name not found or invalid UUID.");
-        }
-    }
-
-    public static String getUserName(String uuid) {
+    public static String getUsernameFromUUID(String uuid) {
         try {
-            return getUsernameFromUUID(uuid);
-        } catch (Exception e) {
-            e.printStackTrace();
+            String urlString = "https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.replace("-","");
+            URL url = new URL(urlString);
+
+            HttpURLConnection connection = null;
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            // レスポンスコードを確認
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                return null;
+            }
+
+            InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+            JsonObject responseJson = JsonParser.parseReader(reader).getAsJsonObject();
+
+            // プロファイル名が存在するか確認
+            if (responseJson.has("name")) {
+                return responseJson.get("name").getAsString();
+            } else {
+                return null;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return "";
     }
 }
