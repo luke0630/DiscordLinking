d704488aa060777b692076547a1cbf4a4741c85a
luke0630 <ryuto-0915@outlook.jp>
2025-02-27 22:57:31 +0900

    update: use unlinkMinecraftAccount

--- src/main/java/org/luke/discordLinking/Command/Command_Unlink.java
+++ src/main/java/org/luke/discordLinking/Command/Command_Unlink.java
@@ -3,11 +3,7 @@ package org.luke.discordLinking.Command;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.proxy.Player;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
-import org.luke.discordLinking.Data;
-import org.luke.discordLinking.DiscordSide.DiscordBot;
-import org.luke.discordLinking.DiscordSide.DiscordBotEvent;
-import org.luke.discordLinking.SQL.SQLManager;
+import org.luke.discordLinking.SQL.SQLUtility;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -16,18 +12,9 @@ public class Command_Unlink implements SimpleCommand {
     @Override
     public void execute(Invocation invocation) {
         CommandSource source = invocation.source();
-        if(source instanceof Player player) {
-            if(SQLManager.isLinkedUser(player.getUniqueId())) {
-                Data.LinkedData linkedData = SQLManager.getLinkedData(player.getUniqueId());
-                Long discordLongID = linkedData.getDiscordUserID();
-
-                DiscordBotEvent.Unlink(player.getUniqueId() ,discordLongID);
-                DiscordBot.getUserById(discordLongID, user -> {
-                    user.openPrivateChannel().queue((MessageChannel channel) -> {
-                        channel.sendMessage("あなたのDiscordアカウントとマインクラフトアカウント(" + player.getUsername() +")とのリンクは解除されました。").queue();
-                    });
-                });
-            }
+        if(source instanceof  Player player) {
+            String discordID = SQLUtility.getDiscordIdByUUID(player.getUniqueId());
+            SQLUtility.unlinkMinecraftAccount(Long.valueOf(discordID), player);
         }
     }
 
