f5af0f960326717f3e5cdf4ad8577eff8aacf9e7
luke0630 <ryuto-0915@outlook.jp>
2025-03-04 16:05:17 +0900

    update: connection変数はprivateにしておく

--- src/main/java/org/luke/discordLinking/SQL/SQLManager.java
+++ src/main/java/org/luke/discordLinking/SQL/SQLManager.java
@@ -9,7 +9,7 @@ import static org.luke.discordLinking.DiscordLinking.getInstance;
 
 public class SQLManager {
 
-    static Connection connection = null;
+    private static Connection connection = null;
     static final String tableName = "linked_data";
     static final String column_discordID = "discord_id";
     static final String column_linked_data = "link_data";
